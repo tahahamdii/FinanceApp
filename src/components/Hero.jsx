57dac5818e5eca70f1be06f2446e5b849a361226
Med Taha Hamdi <hamdi.taha@esprit.tn>
2023-12-26 00:56:14 +0100

    buttons

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
             <p className='text-[#00df9a] font-bold p-2'>GROWING TOGETHER</p>
             <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>HOUSE MUSIC IS STILL KICKING</h1>
             <div>
-                <p className='md:text-3xl sm:text-2xl text-xl '>Take a deep breath and Enjoy</p>
+                <p className='md:text-3xl sm:text-2xl text-xl py-4'>Take a deep breath and Enjoy</p>
                 <TypeAnimation sequence={['We make Money',1000,
             'We make Love', 1000,
             'We make what we want',4000
@@ -16,9 +16,11 @@ const Hero = () => {
             wrapper='span'
             speed={50}
             repeat={Infinity}
-
+            className='md:text-2xl sm:text-1xl text-xl font-bold mt-5'
             />
             </div>
+            <button className='bg-[#00df9a] rounded-md w-[200px] font-medium my-6 mx-auto py-3 text-black '>Get Started</button>
+
         </div>
     </div>
   )
