1611e6ebda0a54b16ea95b5516f415e0b1a07d76
Med Taha Hamdi <hamdi.taha@esprit.tn>
2023-12-26 01:30:21 +0100

    analytics completed

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -7,10 +7,11 @@ const Analytics = () => {
             <img className='w-[500px] mx-auto my-4' src={Laptop} alt='/' />
             <div className='flex flex-col justify-center'>
                 <p className='text-[#00df9a] font-bold'>DATA ANALYTICS DASHBOARD</p>
-                <h1>Manage Data Analytics Centrally</h1>
-                <p>
+                <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>Manage Data Analytics Centrally</h1>
+                <p className=''>
                 Utilisez le text generator online pour créer votre propre texte! Le générateur de texte Lorem Ipsum crée un texte fictif, faux, causal ou fictif.
                 </p>
+                <button className='text-[#00df9a] rounded-md w-[200px] font-medium my-6 mx-auto md:mx-0 py-3 bg-black '>Get Started</button>
             </div>
         </div>
     </div>
